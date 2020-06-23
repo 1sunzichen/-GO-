b7785cbee283d4c1fc92afaca67912d3b53ef7e9
1sunzichen <3023493319@qq.com>
2020-06-23 22:47:07 +0800

    '函數返回值在後面返回多個值函數值編程'

--- part1/28.go
+++ part1/28.go
@@ -1,6 +1,9 @@
 package main
 import(
 	"fmt"
+	"reflect"
+	"runtime"
+	"math"
 )
 func eval(a,b int,op string) (int,error){
 	switch op {
@@ -24,9 +27,28 @@ func div(a,b int)(q,r int){
 	r=a%b
 	return 
 }
+func apply(op func(int,int) int,a,b int) int{
+	p :=reflect.ValueOf(op).Pointer()
+	opName :=runtime.FuncForPC(p).Name()
+	fmt.Printf("CAll"+"(%d,%d)",opName,a,b)
+	return op(a,b)
+}
+func pow(a,b int) int{
+	return int(math.Pow(float64(a),float64(b)))
+}
+
+func sum(numbers ...int) int{
+	s :=0
+	for i:= range numbers{
+		s+=numbers[i]
+	}
+	return s
+}
 func main(){
 	fmt.Println(eval(3,4,"*"))
 	q,r:=div(13,3)
 
 	fmt.Println(q,r)
+	fmt.Println(apply(pow,3,4))
+	fmt.Println(sum(1,2,3,4))
 }
