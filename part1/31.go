8b870f9ff0876988ab30c0d9c4d52f6604a08b47
1sunzichen <3023493319@qq.com>
2020-06-26 22:39:20 +0800

    '數組指針函數'

--- part1/31.go
+++ part1/31.go
@@ -3,7 +3,7 @@ import "fmt"
 func main(){
 	var arr1 [5] int
 	arr2:=[3] int{1,3,5}
-	arr3:=[...]int{2,4,6,8,10}
+	arr3:=[...]int{2,4,6,8,110}
 	// 4 行 5 列
 	var grid [4][5]int
 	fmt.Println(arr1,arr2,arr3)
