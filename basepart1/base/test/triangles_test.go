20468e6d6abb5aca8f72b82ea8a07d8b3cc03048
1sunzichen <3023493319@qq.com>
2020-08-17 20:59:44 +0800

    'commit'

--- basepart1/base/test/triangles_test.go
+++ basepart1/base/test/triangles_test.go
@@ -6,9 +6,9 @@ func TestTriangles(t *testing.T){
 	tests:=[]struct{
 		a,b,c int
 	}{
-		{3,4,5},
-		{12,5,13},
-		{9,12,15},
+		//{3,4,5},
+		//{12,5,13},
+		{3000000000,4000000000,5000000000},
 	}
 	for _,tt:=range tests{
 		if actual:=calctriangle(tt.a,tt.b);
@@ -18,4 +18,26 @@ func TestTriangles(t *testing.T){
 				tt.a,tt.b,actual,tt.c)
 		}
 	}
+}
+func BenchmarkSubstr(b *testing.B){
+	//tests:=[]struct{
+	//	a,b,c int
+	//}{
+	//	//{3,4,5},
+	//	//{12,5,13},
+	//	{3000000000,4000000000,5000000000},
+	//}
+	s:=[3] int{3,4,5}
+	for i:=0;i<b.N;i++{
+
+
+			if actual:=calctriangle(s[0],s[1]);
+				actual!=s[2]{
+				b.Errorf("calcTrianele(%d,%d);"+
+					"got %d; expected %d",
+					s[0],s[1],actual,s[2])
+			}
+
+	}
+
 }
